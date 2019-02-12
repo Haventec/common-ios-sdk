84f54a473435f49d3be92ca712f252b133c84196
Clifford Phan <clifford.phan@haventec.com>
2019-02-12 13:13:11 +1100

    Update generateSalt to return byte array

--- Example/HaventecCommon/ViewController.swift
+++ Example/HaventecCommon/ViewController.swift
@@ -68,20 +68,20 @@ class ViewController: UIViewController, NSURLConnectionDataDelegate {
     
     @IBAction func addDevice() {
         let url = URL(string: serverUrl + "/self-service/device")!
-        
+
         let jsonString: String = "{"
             + "\"applicationUuid\": \"" + applicationUuid + "\","
             + "\"username\": \"" + haventecUsername + "\","
             + "\"email\": \"" + haventecEmail + "\","
             + "\"deviceName\": \"iOS Device\""
             + "}";
-        
+
         var request = URLRequest(url: url)
         request.setValue("application/json", forHTTPHeaderField: "Content-type")
         request.setValue(apiKey, forHTTPHeaderField: "x-api-key")
         request.httpMethod = "POST"
         request.httpBody = jsonString.data(using: .utf8)
-        
+
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             guard let data = data, error == nil else {                                                 // check for fundamental networking error
                 DispatchQueue.main.async {
@@ -90,13 +90,13 @@ class ViewController: UIViewController, NSURLConnectionDataDelegate {
                 print("error=\(error!)")
                 return
             }
-            
+
             let responseString = String(data: data, encoding: .utf8)
             DispatchQueue.main.async {
                 self.addDeviceResponse.text = responseString
                 let jsonData = responseString!.data(using: .utf8)!
                 let decoder = JSONDecoder()
-                
+
                 do {
                     let response = try decoder.decode(AddDeviceResponse.self, from: jsonData)
                     self.deviceUuid = response.deviceUuid
@@ -108,16 +108,16 @@ class ViewController: UIViewController, NSURLConnectionDataDelegate {
             }
             print("responseString = \(responseString!)")
         }
-        
+
         task.resume()
     }
     
     @IBAction func activateDevice() {
-        guard let salt = try? HaventecCommon.generateSalt(size: 128) else { return }
-        let hashedPin: String = HaventecCommon.hashPin(salt: salt, pin: pinCode)
-        
+        guard let saltBytes: [UInt8] = try? HaventecCommon.generateSalt() else { return }
+        let hashedPin: String = HaventecCommon.hashPin(saltBytes: saltBytes, pin: pinCode)
+
         let url = URL(string: serverUrl + "/authentication/activate/device")!
-        
+
         let jsonString: String = "{"
             + "\"applicationUuid\": \"" + applicationUuid + "\","
             + "\"username\": \"" + haventecUsername + "\","
@@ -125,13 +125,13 @@ class ViewController: UIViewController, NSURLConnectionDataDelegate {
             + "\"hashedPin\": \"" + hashedPin + "\","
             + "\"activationToken\": \"" + activationToken + "\""
             + "}";
-        
+
         var request = URLRequest(url: url)
         request.setValue("application/json", forHTTPHeaderField: "Content-type")
         request.setValue(apiKey, forHTTPHeaderField: "x-api-key")
         request.httpMethod = "POST"
         request.httpBody = jsonString.data(using: .utf8)
-        
+
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             guard let data = data, error == nil else {                                                 // check for fundamental networking error
                 DispatchQueue.main.async {
@@ -140,13 +140,13 @@ class ViewController: UIViewController, NSURLConnectionDataDelegate {
                 print("error=\(error!)")
                 return
             }
-            
+
             let responseString = String(data: data, encoding: .utf8)
             DispatchQueue.main.async {
                 self.activateDeviceResponse.text = responseString
                 let jsonData = responseString!.data(using: .utf8)!
                 let decoder = JSONDecoder()
-                
+
                 do {
                     let response = try decoder.decode(ActivateDeviceResponse.self, from: jsonData)
                     self.authKey = response.authKey
@@ -158,7 +158,7 @@ class ViewController: UIViewController, NSURLConnectionDataDelegate {
             }
             print("responseString = \(responseString!)")
         }
-        
+
         task.resume()
     }
 
--- HaventecCommon/Classes/HaventecCommon.swift
+++ HaventecCommon/Classes/HaventecCommon.swift
@@ -2,21 +2,24 @@ import Foundation
 import CommonCrypto
 
 public class HaventecCommon {
+    static let saltByteSize: Int = 128
+    
     enum HaventecError: Error {
-        case generateSalt
+        case generateSalt(String)
+        case hashPin(String)
     }
     
-    public static func generateSalt(size: Int) throws -> String {
+    public static func generateSalt() throws -> [UInt8] {
         var saltArray: [Int32] = []
         
-        for _ in 0..<size {
+        for _ in 0..<saltByteSize {
             var intOut: Int32 = 0;
             
             if let dataBytes = try? generateBytes(length: 4), let data = dataBytes {
                 data.getBytes(&intOut, length: MemoryLayout<Int32>.size)
                 saltArray.append(intOut);
             } else {
-                throw HaventecError.generateSalt
+                throw HaventecError.generateSalt("Error generating random bytes")
             }
         }
         
@@ -33,26 +36,37 @@ public class HaventecCommon {
             saltString += word;
         }
         
-        var returnString: String = ""
-        if let data = saltString.data(using: .utf8) {
-            returnString = data.base64EncodedString()
-        }
+        guard let rawSaltBytes = saltString.data(using: .utf8) else { throw HaventecError.generateSalt("Error encoding the raw string into bytes") }
         
-        return returnString
+        if let encodedSaltBytes = rawSaltBytes.base64EncodedString().data(using: .utf8) {
+            return Array(encodedSaltBytes)
+        } else {
+            throw HaventecError.generateSalt("Error encoding the generated salt with given charset")
+        }
     }
     
-    public static func hashPin(salt: String, pin: String) -> String {
+    public static func hashPin(saltBytes: [UInt8], pin: String) -> String {
+        let salt: String = String(bytes: saltBytes, encoding: .utf8)!
         var basePin: String = ""
+        
         if let dataPin = salt.data(using: .utf8) {
             basePin = dataPin.base64EncodedString()
         }
-        
+
         let saltString: String = salt + basePin;
-        
+
         let shaString: String = sha512Base64(instring: saltString)
-        
+
         return shaString
     }
+    
+//    private func sha512(string string: String) -> [UInt8] {
+//        var digest = [UInt8](count: Int(CC_SHA512_DIGEST_LENGTH), repeatedValue: 0)
+//        let data = string.dataUsingEncoding(NSUTF8StringEncoding)!
+//        CC_SHA512(data.bytes, CC_LONG(data.length), &digest)
+//
+//        return digest
+//    }
     
     private static func generateBytes(length : Int) throws -> NSData? {
         var bytes = [Int32](repeating: Int32(0), count: length)
@@ -63,14 +77,16 @@ public class HaventecCommon {
         return NSData(bytes: bytes, length: bytes.count)
     }
     
+    
+    
     private static func sha512Base64(instring: String) -> String {
         let digest = NSMutableData(length: Int(CC_SHA512_DIGEST_LENGTH))!
         if let data = instring.data(using: String.Encoding.utf8) {
-            
+
             let value =  data as NSData
             let uint8Pointer = UnsafeMutablePointer<UInt8>.allocate(capacity: digest.length)
             CC_SHA512(value.bytes, CC_LONG(data.count), uint8Pointer)
-            
+
         }
         return digest.base64EncodedString(options: NSData.Base64EncodingOptions([]))
     }
